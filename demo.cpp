4fc8602a3f96ff280e6695f1678a1cedff8e5cc4
Carl Edquist <edquist@cs.wisc.edu>
2020-12-11 11:32:15 -0600

    more demos
    - break out for loop things to template functions
    - add example with string "container" with end sentinel

--- demo.cpp
+++ demo.cpp
@@ -2,32 +2,72 @@
 #include <initializer_list>
 #include <iostream>
 
-void demo1()
+template <class ...Arg>
+void print_range_for_pairs(Arg &&...arg)
 {
-	auto list = { 1,2,3,4,99 };
-	for (auto &&x : all_pairs(list) )
+	for (auto &&x : all_pairs(std::forward<Arg>(arg)...) )
 		std::cout << x.first << "\t" << x.second << "\n";
 }
 
-void demo2()
+template <class ...Arg>
+void print_range_for_pairs2(Arg &&...arg)
 {
 	// c++17 - range-for loop semantics (begin/end can be different types)
 
-	auto list = { 1,2,3,4,99 };
-	auto &&range = all_pairs(list);
-	auto begin = range.begin();
-	auto end = range.end2();  // the end sentinal version
+	auto &&range = all_pairs(std::forward<Arg>(arg)...);
+	auto begin   = range.begin();
+	auto end     = range.end2();  // the end sentinal version
 	for ( ; begin != end; ++begin) {
 		auto &&x = *begin;
 		std::cout << x.first << "\t" << x.second << "\n";
 	}
 }
 
+
+void demo1()
+{
+	auto list = { 1,2,3,4,99 };
+	print_range_for_pairs(list);
+}
+
+void demo2()
+{
+	auto list = { 1,2,3,4,99 };
+	print_range_for_pairs2(list);
+}
+
+void demo3()
+{
+	auto list = { 'a', 'b', 'c', 'd' };
+	print_range_for_pairs(list);
+}
+
+void demo4()
+{
+	const char *s = "abcd";
+	print_range_for_pairs(s, s+4);
+}
+
+struct cstr_end {};
+
+bool operator==(const char *p, cstr_end) { return *p == 0; }
+bool operator!=(const char *p, cstr_end) { return *p != 0; }
+
+
+void demo5()
+{
+	// all pairs for string with end sentinal
+	print_range_for_pairs2("abcd", cstr_end{});
+}
+
+void spacer() { std::cout << "\n----------\n\n"; }
+
 int main()
 {
-	demo1();
-
-	std::cout << "\n----------\n\n";
-
-	demo2();
+	demo1(); spacer();
+	demo2(); spacer();
+	demo3(); spacer();
+	demo4(); spacer();
+	demo5(); spacer();
 }
+

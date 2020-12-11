c96f294ffdf5aaeda31283648495fb3f6eb05f76
Carl Edquist <edquist@cs.wisc.edu>
2020-12-10 21:45:17 -0600

    cleanup

--- all_pairs.h
+++ all_pairs.h
@@ -2,26 +2,26 @@
 
 #include <utility>
 
-// namespace ltd { }
-
 
 template <class P, class IT>   // P ~ std::pair<IT, IT>, w/o refs
-P next_pair(P prev_pair, IT &&end)
+P next_pair(P pair, IT &&end)
 {
-	if (++prev_pair.second == end) {
-		prev_pair.second = ++prev_pair.first;
-		++prev_pair.second;
-		if (prev_pair.second == end)  // not needed for sentinal version
-			prev_pair.first = end;
+	if (++pair.second == end) {
+		pair.second = ++pair.first;
+		++pair.second;
+		if (pair.second == end)  // not needed for sentinal version
+			pair.first = end;
 	}
-	return prev_pair;
+	return pair;
 }
 
-template <class P, class E2>
-P &advance_pair(P &prev_pair, E2 &&e2)
+
+template <class P, class IT>
+P &advance_pair(P &prev_pair, IT &&end)
 {
-	return next_pair<P &, E2>(prev_pair, e2);
+	return next_pair<P &, IT>(prev_pair, std::forward<IT>(end));
 }
+
 
 template <class IT>
 struct all_pairs_t {
@@ -37,9 +37,6 @@ struct all_pairs_t {
 		typedef decltype(*end) item_type;
 		typedef std::pair<item_type, item_type> value_type;
 
-		//value_type &operator*()
-		//{ return {*pos.first, *pos.second}; }
-
 		value_type  operator*() const
 		{ return {*pos.first, *pos.second}; }
 
@@ -47,6 +44,7 @@ struct all_pairs_t {
 			advance_pair(pos, end);
 			return *this;
 		}
+
 
 		bool operator==(const iterator &o) const
 		{ return pos == o.pos; }
@@ -69,15 +67,15 @@ struct all_pairs_t {
 	end_iterator end2()  const { return {}; }  // c++17
 };
 
+
 template <class IT>
 all_pairs_t<IT>
 all_pairs(IT begin, IT end)
 { return {begin, end}; }
 
+
 template <class C>
 auto all_pairs(C &&c)
-   -> decltype(all_pairs(c.begin(), c.end()))
-{
-	return all_pairs(c.begin(), c.end());
-}
+  -> decltype(all_pairs(c.begin(), c.end()))
+{ return      all_pairs(c.begin(), c.end()); }
 
--- demo.cpp
+++ demo.cpp
@@ -11,12 +11,12 @@ void demo1()
 
 void demo2()
 {
-	// c++17 - style range-for loop (begin/end can be different types)
+	// c++17 - range-for loop semantics (begin/end can be different types)
 
 	auto list = { 1,2,3,4,99 };
 	auto &&range = all_pairs(list);
 	auto begin = range.begin();
-	auto end = range.end2();
+	auto end = range.end2();  // the end sentinal version
 	for ( ; begin != end; ++begin) {
 		auto &&x = *begin;
 		std::cout << x.first << "\t" << x.second << "\n";
